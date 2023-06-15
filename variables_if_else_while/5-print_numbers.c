fcc9af42577fae66703ae9f4bb27205f2ddea7ad
FacundoV21 <6756@exampleholbertonstudents.com>
2023-06-15 15:17:01 -0700

    task5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Prints a line with the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar (n);
+	}
+	putchar ('\n');
+	return (0);
+}
