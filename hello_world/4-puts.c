6143138afa1630af82be9b3041018ebcd4f1115f
FacundoV21 <6756@exampleholbertonstudents.com>
2023-06-14 17:30:49 -0700

    task4fix4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
-/*
+/**
  * main -Shows the line \"Programming is like building
- * a multilingual puzzle\" followed by a new line
+ *a multilingual puzzle\" followed by a new line
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
