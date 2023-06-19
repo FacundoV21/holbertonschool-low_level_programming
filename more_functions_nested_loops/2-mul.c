aee9c6d129513f44f10e4ecafcca4952bf89333a
FacundoV21 <6756@exampleholbertonstudents.com>
2023-06-19 15:05:10 -0700

    task2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - Returns the result of a times b
+ * @a: first input
+ * @b: second input
+ * Return: - result of a times b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
