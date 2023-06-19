617abde7109bf0ac56203e662cfbb8da5251b0a5
FacundoV21 <6756@exampleholbertonstudents.com>
2023-06-19 16:47:40 -0700

    task6fix

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -8,14 +8,12 @@ void print_line(int n)
 {
 	int c;
 
-	for (c = 0; c <= n; c++)
+	for (c = 1; c <= n; c++)
 	{
-			if (n > 0)
-			{
-				_putchar('_');
-			}
-			
+		if (n > 0)
+		{
+			_putchar('_');
+		}
 	}
 	_putchar('\n');
-	
 }
