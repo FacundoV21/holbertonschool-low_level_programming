dd825445278baea760561066a6d98c953c10c085
FacundoV21 <6756@exampleholbertonstudents.com>
2023-06-19 15:41:24 -0700

    task4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints nums from 0 to 9
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		if (c != 4 && c != 2)
+		_putchar(c + '0');
+	}
+	_putchar('\n');
+}
