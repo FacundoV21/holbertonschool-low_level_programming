fdb807533cbbcc56df786d040929ae2f4a82b64c
FacundoV21 <6756@exampleholbertonstudents.com>
2023-06-20 06:12:58 -0700

    task9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+/**
+ * fizz_buzz - Prints nums from 1 to 100 replacing multiples of 5 with Buzz
+ * and multiples of 3 with Fizz and the both with FizzBuzz
+ * Return: always 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 1; c <= 100; c++)
+	{
+		if (c != 1)
+		{
+			putchar(' ');
+		}
+		if (((c % 3) == 0) && ((c % 5) == 0))
+		{
+		printf("FizzBuzz");
+		}
+		else if (((c % 3) == 0) && ((c % 5) != 0))
+		{
+			printf("Fizz");
+		}
+		else if (((c % 3) != 0) && ((c % 5) == 0))
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
