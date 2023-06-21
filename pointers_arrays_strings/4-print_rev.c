60da1ef5ee3ec35f859f3e9efb88824a5e80ce06
FacundoV21 <6756@exampleholbertonstudents.com>
2023-06-21 14:42:25 -0700

    task4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: String input
+ */
+
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	for (; count >= 0; count--)
+	{
+		_putchar(s[count]);
+	}
+	_putchar('\n');
+}
