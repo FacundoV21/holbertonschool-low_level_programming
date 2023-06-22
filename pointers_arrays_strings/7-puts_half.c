c45ecbe22a9f8add619e984c1ec51807a148d218
FacundoV21 <6756@exampleholbertonstudents.com>
2023-06-22 04:55:53 -0700

    task7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -10,11 +10,10 @@ void puts_half(char *str)
 {
 	int n, i, c = 0;
 
-	while (str[c] != '\0')
+	while (str[c] != '\0' && str[c + 1] != '\0')
 	{
 		c++;
 	}
-
 	if (c % 2 != 0)
 	{
 		n = (c - 1) / 2;
@@ -23,9 +22,9 @@ void puts_half(char *str)
 	{
 		n = c / 2;
 	}
-	for (i = 0; i < c; i++)
+	for (i = 0; i <= c; i++)
 	{
-		if (i >= n)
+		if (i > n)
 		{
 			_putchar(str[i]);
 		}
