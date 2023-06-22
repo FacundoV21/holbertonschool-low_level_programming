4aa8175336db6a187d25eec58110906242778151
FacundoV21 <6756@exampleholbertonstudents.com>
2023-06-22 07:19:54 -0700

    task9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strcpy: copies the content of a string to another
+ * @*dest: destination input
+ * @*src: source input
+ * @dest: array where you put chars
+ * @src: array where you get your chars from
+ * Return: the pointer to dest.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	for (i = 0; src[i - 1] != '\0'; i++)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	return (dest);
+}
