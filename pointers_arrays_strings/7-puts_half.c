0666cd7ae2d9140e05bbe4b43f07d0531a97d6f9
FacundoV21 <6756@exampleholbertonstudents.com>
2023-06-22 04:35:08 -0700

    task7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half a string
+ * @str: String input
+ *Return: always 0
+ */
+
+void puts_half(char *str)
+{
+	int n, i, c = 0;
+
+	while (str[c] != '\0')
+	{
+		c++;
+	}
+
+	if (c % 2 != 0)
+	{
+		n = (c - 1) / 2;
+	}
+	else
+	{
+		n = c / 2;
+	}
+	for (i = 0; i <= c; i++)
+	{
+		if (i >= n)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
