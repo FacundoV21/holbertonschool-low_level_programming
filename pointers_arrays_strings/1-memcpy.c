3fa0ba144b675346bad85293cbde3b27f612720b
FacundoV21 <6756@exampleholbertonstudents.com>
2023-06-23 09:08:41 -0700

    task001

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies part of a string to another
+ * @*src: source string input
+ * @*dest: destination string input
+ * @src: value of *src
+ * @dest: value of *dest
+ * @n: number of bytes to edit
+ * Return: pointer to string
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
