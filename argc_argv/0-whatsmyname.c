b6135afb3c2598edf1169991674f1e640b994d45
FacundoV21 <6756@exampleholbertonstudents.com>
2023-06-30 07:24:31 -0700

    task0bettyfix

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,12 +3,13 @@
 /**
  * main - Prints the program name
  * @argc: amount of arguments
- * @* argv[]: array of arguments
+ * @*argv[]: array of arguments
+ * @argv: value of *argv
  * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[argc-1]);
-	return(0);
+	printf("%s\n", argv[argc - 1]);
+	return (0);
 }
