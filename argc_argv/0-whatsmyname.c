bebba6e6bdd011763ac8104dc45ab16260cd000f
FacundoV21 <6756@exampleholbertonstudents.com>
2023-06-30 07:17:18 -0700

    task0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the program name
+ * @argc: amount of arguments
+ * @* argv[]: array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc-1]);
+	return(0);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,1 @@
+Edit once finished
