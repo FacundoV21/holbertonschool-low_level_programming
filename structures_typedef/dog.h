ac2b504183087e4e555010bae72a232f0f6d82fe
FacundoV21 <6756@exampleholbertonstudents.com>
2023-07-10 06:59:27 -0700

    task0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/**
+ * struct dog - dog
+ * @name: name of the dog
+ * @age: age "          "
+ * @owner: owner "      "
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
