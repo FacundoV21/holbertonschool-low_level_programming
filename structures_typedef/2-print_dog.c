cff69c28edd30ca99d0622c85fb1a5dd1c4586c4
FacundoV21 <6756@exampleholbertonstudents.com>
2023-07-11 18:49:52 -0700

    task2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - Write a function that prints a dog's characteristics
+ * @d: Struct to be printed
+ * Return: Nothing
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %.6f\n", d->age);
+
+	if (d->owner == NULL)
+	printf("Owner: (nil)\n");
+	else
+
+	printf("Owner: %s\n", d->owner);							
+}
