b16c53efcb4ea3de3f7dad31792547473e9aeecb
FacundoV21 <6756@exampleholbertonstudents.com>
2023-07-11 18:55:22 -0700

    task3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -13,10 +13,10 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog *new_dog(char *name, float age, char *owner);
-void free_dog(dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
