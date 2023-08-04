87827ae6b532e89038630fb36db718a603e176af
FacundoV21 <6756@holbertonstudents.com>
2023-08-04 02:57:20 +0000

    feat: function to free a hash table

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * hash_table_print - deletes a hash table
+ * @ht: hash table to delete
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int ub = 0;
+	hash_node_t *move, *stay;
+
+	while (ht != NULL && ub < ht->size)
+	{
+		move = ht->array[ub];
+		while (move != NULL)
+		{
+			stay = move;
+			move = move->next;
+			free(stay->value);
+			free(stay->key);
+			free(stay);
+		}
+		ub++;
+	}
+	free(ht->array);
+	free(ht);
+}
