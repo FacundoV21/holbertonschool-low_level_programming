a96ece6834fe45f8ace9447f3f0af9d8a71f5912
FacundoV21 <6756@holbertonstudents.com>
2023-09-18 00:09:09 +0000

    fixed betty

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * linear_search - searches for a value in an array of integers using the 
+ * linear_search - searches for a value in an array of integers using the
  * Linear search algorithm
  * @array: array of integers
  * @size: size of array
